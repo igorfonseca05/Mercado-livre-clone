684204c55090fa1408c3ed6607196b218bd8914d
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-01-04 11:43:22 -0300

    alterei folha de estilo

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ const createslide = () => {
     const slidec= document.querySelector('.slidec')
 
     const imagens = [
-        { id: 1, url: "img/1.JPEG" },
-        { id: 2, url: "img/2.JPEG" },
-        { id: 3, url: "img/3.JPEG" },
-        { id: 4, url: "img/4.JPEG" },
+        { id: 1, url: "img/1.jpeg" },
+        { id: 2, url: "img/2.jpeg" },
+        { id: 3, url: "img/3.jpeg" },
+        { id: 4, url: "img/4.jpeg" },
         { id: 5, url: "img/5.jpeg" },
     ]
 
